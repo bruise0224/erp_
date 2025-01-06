c262a1d2013418c12316b2528ba5479f8f5ac494
bruise0224 <1985438967@qq.com>
2025-01-06 14:10:23 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+  ,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ]
+  },
+  devServer: {
+    port: 8080,
+    client: {
+      overlay: false
+    },
+  }
+})
